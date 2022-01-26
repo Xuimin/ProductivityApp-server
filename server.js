da841123e932ee40cfbff965474a58f4c6afc192
Xuimin <xuimin.kt27@gmail.com>
2022-01-26 09:35:10 +0800

    Added DB_PASSWORD

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 // const { DB_NAME, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
-const { PORT } = process.env
+const { PORT, DB_PASSWORD } = process.env
 
 app.use(cors())
 app.use(express.json())
@@ -13,7 +13,7 @@ app.use(express.json())
 // mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
 
 // ONLINE DATABASE
-mongoose.connect("mongodb+srv://xuimin:xuimin123@productivityapp.lxyzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect(`mongodb+srv://xuimin:${DB_PASSWORD}@productivityapp.lxyzi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 mongoose.connection.once('open', () => console.log("Connected to MongoDB"))
 
 app.use(express.static('Public')) // http://localhost:5000/Public/imagename.jpg
